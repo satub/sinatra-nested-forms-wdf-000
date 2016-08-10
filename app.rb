ffc14d8173ace590f8a4cddcdd17572cfb9dd83e
satub <satu.barnhill@gmail.com>
2016-08-10 16:38:28 -0400

    Removed unneccessary comments

--- app.rb
+++ app.rb
@@ -18,12 +18,6 @@ module FormsLab
         @ship = Ship.new(ship["name"], ship["type"], ship["booty"])
         @pirate.ships << @ship
       end
-      #  binding.pry
-      # @ship1 = Ship.new(params["ship_name_1"], params["ship_type_1"], params["ship_booty_1"])
-      # @ship1 = Ship.new(params["ship_name_2"], params["ship_type_2"], params["ship_booty_2"])
-      # @pirate.ships << @ship1
-      # @pirate.ships << @ship2
-
       erb :"pirates/show"
     end
 
